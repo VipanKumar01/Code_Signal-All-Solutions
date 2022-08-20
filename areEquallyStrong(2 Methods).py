8d296cf72c9a127c575aebb8ae162e11c0af3f4a
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-08-20 22:21:59 +0530

    Create areEquallyStrong(2 Methods).py

---
+++ areEquallyStrong(2 Methods).py
@@ -0,0 +1,21 @@
+# Name = Vipan Kumar
+# GitHub user name = @VipanKumar01
+
+def solution(yourLeft, yourRight, friendsLeft, friendsRight):
+    if yourLeft + yourRight == friendsLeft + friendsRight and (yourLeft == friendsLeft or yourLeft == friendsRight):
+        return True
+    else:
+        return False
+      
+      
+      
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Name = Vipan Kumar
+# GitHub user name = @VipanKumar01
+
+def solution(yourLeft, yourRight, friendsLeft, friendsRight):
+    return yourLeft + yourRight == friendsLeft + friendsRight and (yourLeft == friendsLeft or yourLeft == friendsRight)
+      
+      
+# --HappyCode--
